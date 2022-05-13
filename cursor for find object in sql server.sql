5947d6e19f6d458c8e0e061b2c0399a2ee49dc9e
moosavii <85693212+moosavii@users.noreply.github.com>
2022-05-13 11:15:37 +0430

    Add files via upload

---
+++ cursor for find object in sql server.sql
@@ -0,0 +1,35 @@
+﻿declare @dbname varchar(max)
+		, @query varchar(max)
+		, @searchItem varchar(max)
+
+declare cursor1 scroll cursor
+for
+	select '[' + [name] + ']'
+	from sys.databases
+	where owner_sid <> 0x01
+
+open cursor1
+	fetch next from cursor1 into @dbname
+	while @@FETCH_STATUS = 0
+		BEGIN
+			PRINT @dbname
+			set @query = ''
+			set @query = ' select ''' + @dbname + ''' , objects.name , objects.type_desc , create_date , modify_date '
+					+ ' from sys.objects '
+					+ ' left join '
+					+ @dbname + '.sys.sql_modules '
+					+ ' on sql_modules.object_id = objects.object_id '
+					+ ' where definition like ''%' + @searchItem + '%'''
+
+			print @query
+			begin try
+				exec(@query)
+			end try
+			begin catch
+				select ERROR_MESSAGE()
+			end catch
+			fetch next from cursor1 into @dbname
+		end
+close cursor1
+deallocate cursor1
+
